090468b2704cc0b76171b30042e6d6819c0b7f44
Prasad More <prasadmoremain@gmail.com>
2023-12-28 17:58:21 +0530

    Displayed Registered Users with Not allowing same mail id to register

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 
 let users = []
 
+let userReg = document.getElementById('regUsers')
+let userTable = () =>{
+    userReg.innerHTML=''
+    for(let i = 0 ; i<users.length;i++){
+
+        let div = document.createElement('div');
+        let name = document.createElement('div');
+        let email = document.createElement('div');
+
+        div.classList.add('registered')
+
+        name.innerText = users[i].name;
+        email.innerText = users[i].email;
+
+        userReg.appendChild(div)
+        div.appendChild(name)
+        div.appendChild(email)
+    }
+
+
+
+}
+
+
 let addUser = () =>{
     let name = document.getElementById('name');
     let mail = document.getElementById('email');
@@ -10,12 +34,13 @@ let addUser = () =>{
     })
 
     let usersobj = {
-        name: name.value,
-        email: mail.value
+        'name': name.value,
+        'email': mail.value
     }
 
     if(userExist.length == 0){
         users.push(usersobj)
+        userTable();
     } 
     else {
         window.alert('User Already Exist')
